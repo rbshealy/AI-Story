243e8f5c6168c4c6e06e6ffa69ecf12544645b2f
Ray Shealy <rayshealy227@gmail.com>
2025-03-03 22:33:50 -0500

    working demo

--- demo.py
+++ demo.py
@@ -1,10 +1,16 @@
+from PIL import Image
 from gradio_client import Client
 
-client = Client("deepseek-ai/Janus-1.3B")
+access_token = "your access token here"
+
+client = Client("deepseek-ai/Janus-Pro-7B", hf_token = access_token)
 result = client.predict(
 		prompt="Hello!!",
 		seed=12345,
 		guidance=5,
 		api_name="/generate_image"
 )
-print(result)
+
+for res in result:
+	test = Image.open(res['image'])
+	test.show()
