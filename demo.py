22c85c8883a7e890e7000114c7c49d72fa4d5554
Ray Shealy <rayshealy227@gmail.com>
2025-03-06 14:38:40 -0500

    Add content to demo.py

--- demo.py
+++ demo.py
@@ -1,16 +1,40 @@
 from PIL import Image
-from gradio_client import Client
+from gradio_client import Client, handle_file
 
-access_token = "your access token here"
+access_token = "Your Access Token Here"
 
 client = Client("deepseek-ai/Janus-Pro-7B", hf_token = access_token)
-result = client.predict(
-		prompt="Hello!!",
-		seed=12345,
+result1 = client.predict(
+		prompt="Gamer revolution takes over the world",
+		seed=123456,
 		guidance=5,
 		api_name="/generate_image"
 )
 
-for res in result:
-	test = Image.open(res['image'])
-	test.show()
+pr = client.predict(
+		image=handle_file(result1[0]['image']),
+		question="Use a storytelling tone to narrate what is going on in the image, but keep it pretty short. Then, make up something that would happen next in this context",
+		seed=42,
+		top_p=0.95,
+		temperature=0.1,
+		api_name="/multimodal_understanding"
+)
+
+im = Image.open(result1[0]['image'])
+im.show()
+
+print(pr)
+
+result2 = client.predict(
+		prompt=pr,
+		seed=123456,
+		guidance=5,
+		api_name="/generate_image"
+)
+
+im2 = Image.open(result2[0]['image'])
+im2.show()
+
+#for res in result1:
+	#test = Image.open(res['image'])
+	#test.show()
